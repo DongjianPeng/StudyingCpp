950e5acaa5f935ccabb834dfc1823e17034a198d
dongjianpeng <pengdongjian@gmail.com>
2018-04-01 22:23:58 +0800

    xx

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+using namespace std;
+
+int result(int,int);
+
+const int k = 2;
+
+struct Point{
+	int x,y;
+};
 
 int main(int argc, char** argv) {
-	std::cout << "Hello world!\n";
+	int z(0),b(50);
+	Point a;
+	cout << "input:";
+	cin >> a.x >> a.y;
+	z = (a.x + a.y) * k;
+	z = result(z,b);
+	cout << "result:" << endl;
+	cout << z << endl;
 }
+
+int result(int a,int b)
+{
+	return a+b;
+}
+
